725cd10068dd0a5d53d595f087de84340146b783
Hamish Starling <hamishstarling@hotmail.co.uk>
2024-02-03 19:55:28 +0000

    Update manifest and script to allow for requests

--- chrome_extension/extension.js
+++ chrome_extension/extension.js
@@ -1,13 +1,23 @@
-function callApi(revising=true, subject='work') {
+// function callApi(revising=true, subject='work') {
     
-    getCurrentUrl()
-    chrome.runtime.sendMessage({ url: getCurrentUrl(), working: revising, typeOfWork: subject }, response => {
-        console.log(response)
+//     getCurrentUrl()
+//     chrome.runtime.sendMessage({ url: getCurrentUrl(), working: revising, typeOfWork: subject }, response => {
+//         console.log(response)
+//     })
+// }
+
+// function getCurrentUrl() {
+//     const currentUrl = window.location.href;
+//     console.log(currentUrl);
+//     return currentUrl;
+// }
+// https://ichack-e82c16304232.herokuapp.com/
+function run() {
+    fetch("https://httpbin.org/post", {
+        method: 'POST'
+    }).then(res => {
+        console.log(res)
     })
 }
 
-function getCurrentUrl() {
-    const currentUrl = window.location.href;
-    console.log(currentUrl);
-    return currentUrl;
-}
+document.getElementById("run").onclick = run 
