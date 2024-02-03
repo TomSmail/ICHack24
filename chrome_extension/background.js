7d8b9a4dcd863b4f8e70724f8051ad2fa4f56c3c
TomSmail <thomas.c.smail@gmail.com>
2024-02-03 22:15:40 +0000

    feat: add open ai preliminary support

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,23 +1,50 @@
-chrome.runtime.onMessage.addListener(function (message, sender, senderResponse) {
-    {
-      fetch('<https://api.openai.com/v1/chat/completions>', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer <<<<$OPENAI_API_KEY>>>>'
-        },
-        body: {
-            'model': 'gpt-3.5-turbo',
-            'messages': [{'role': 'user', 'content': 'only respond yes or no to my next question'}, 
-            {'role': 'user', 'content': 'is this url related to ' + message.typeOfWork + ': ' + message.url  }],
-            'temperature': 0.7
-          }
+// workflow : new tab created -> read tab url -> run request to open ai -> if no close tab. 
 
-      }).then(res => {
-        return res.json();
-      }).then(res => {
-        senderResponse(res);
-      })
-    }
-    return true
+// Listens for a new tab being opened and sends a message to 
+// This should trigger the workflow
+
+OPEN_AI_API_KEY = ''
+
+
+async function callApi(message) {
+    console.log("API key:" + OPEN_AI_API_KEY)
+    console.log("Type of Work " + message.typeOfWork);
+    console.log("URL " + message.url);
+
+    data = JSON.stringify({
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": "only respond yes or no to my next question"}, 
+        {"role": "user", "content": "is this url related to " + message.typeOfWork + ": " + message.url  }],
+        "temperature": 0.7
+    });
+
+    console.log(data);
+
+    fetch( 'https://api.openai.com/v1/chat/completions', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${OPEN_AI_API_KEY}`
+        },
+        body: data
+    })
+    .then(response => response.json())
+    .then(console.log)
+    .catch(error => {
+        throw(error);
+    })
+}
+
+chrome.tabs.onUpdated.addListener((tab) => {
+    console.log("In background listener");
+    main();
   });
+
+function main() {
+    // Query current URL and call API 
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+        let currentUrl = tabs[0].url;
+        message = {typeOfWork: 'physics', url: currentUrl}
+        result = callApi(message)
+    });
+}
