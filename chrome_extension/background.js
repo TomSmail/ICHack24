ab995eb6ba6e80cae57a30c13887ebf5e23d3620
Hamish Starling <hamishstarling@hotmail.co.uk>
2024-02-04 02:50:10 +0000

    Fix Tom's closing

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,18 +1,13 @@
 NOTHING_TO_SEE_HERE='YXdkajl6dlFTcHB5eW5wRklrY3hKRmtibEIzVE5zSFVXSlpTZVZsRkFjanZNYzRxLWtz'
 OPEN_AI_API_KEY = atob(NOTHING_TO_SEE_HERE).toString().split("").reverse().join("")
 
-whiteListedUrls = ["https://chrome", "chrome://extensions/", "https://www.google.com/", "chrome://newtab/", "chrome://extensions/?errors=ngamojokpcaedomjpobibdiieeopibcb"]
-// workflow : new tab created -> read tab url -> run request to open ai -> if no close tab. 
+whiteListedUrls = ["https://chrome", "chrome://extensions/", "https://www.google.com/", "chrome://newtab/", "chrome://extensions/?errors=bmfmjblgbhgonchjepiileeljppdhegf"]
 
 async function callApi(message, tab) {
-    console.log("API key:" + OPEN_AI_API_KEY)
-    console.log("Type of Work " + message.typeOfWork);
-    console.log("URL " + message.url);
-
     data = JSON.stringify({
         "model": "gpt-3.5-turbo",
         "messages": [{"role": "user", "content": "only respond 'yes' or 'no' to my next question"}, 
-        {"role": "user", "content": "can the following url be used to study : " + message.typeOfWork +  " " + message.url }],
+        {"role": "user", "content": "can the following url be used to study?" + message.url }],
         "temperature": 1.0
     });
 
@@ -36,9 +31,12 @@ async function callApi(message, tab) {
 
 // Listens for a new tab being opened and sends a message to 
 // This should trigger the workflow
-chrome.tabs.onUpdated.addListener((tab) => {
-    console.log("In background listener");
-    main(tab);
+chrome.tabs.onUpdated.addListener((tabId, info) => {
+    console.log("In background listener");    
+    if (info.status === 'complete') {
+        console.log(info.status)
+        main(tabId);
+    }
   });
 
 function main(tab) {
@@ -48,7 +46,7 @@ function main(tab) {
         if (whiteListedUrls.includes(currentUrl)) {
             console.log("Whitelisted URL, all is good!");
         } else {
-            message = {typeOfWork: 'physics', url: currentUrl}
+            message = {url: currentUrl}
             result = callApi(message, tab)
         }
         
@@ -59,7 +57,7 @@ function interpretJson(json, tab) {
     console.log("json:" + json);
     console.log("choices: " + json.choices[0].message.content);
     answer = (json.choices[0].message.content).toLowerCase();
-    if (answer === "no" | answer === "no." ) {
+    if (answer === "no" || answer === "no." ) {
         console.log(answer);
         console.log("CLOSE TAB: " + tab);
         chrome.tabs.remove(tab, function() {
